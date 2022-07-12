94787520afd08970280f9b21479e63f12df8a7c2
Meng Po <pengitoast@gmail.com>
2022-07-12 01:33:00 -0600

    fixed a bit of a syntax issue that caused the code to break

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 import Deck from "./deck.js"
 
 const computerCardSlot = document.querySelector('.computer-card-slot')
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const playerCardSlot = document.querySelector('.player-card-slot')
+const text = document.querySelector('.text')
 
-let playerDeck, computerDeck
+let playerDeck, computerDeck;
 
-startGame
+startGame()
 function startGame() {
-    const deck = new Deck()
-    deck.shuffle()
+  const deck = new Deck()
+  deck.shuffle()
 
-    const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+  const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
 
     console.log(playerDeck)
     console.log(computerDeck)
+    cleanBeforeRound()
+}
+
+function cleanBeforeRound() {
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innerHTML = ''
+    text.innerText = ''
+
+    updateDeckCount()
+}
+
+function updateDeckCount() {
+    computerDeckElement.innerText = computerDeck.numberOfCards
+    playerDeckElement.innerText = playerDeck.numberOfCards
 }
