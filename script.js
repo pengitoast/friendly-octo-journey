ec6546bc5409a13e5dedf01e59ed4a665fd6666e
Meng Po <pengitoast@gmail.com>
2022-07-17 22:24:55 -0600

    added a CARD_VALUE_MAP object which equates strings to numerical value so that when cards are drawn, the value. also added if/else statements to the flipCards function which determines the winner of a round and then adds both cards to their deck

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 import Deck from "./deck.js"
 
+//CARD_VALUE_MAP is an object that is used to compare card values; in the card game 'War', the higher value takes all and adds to their deck.
+const CARD_VALUE_MAP = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+}
+
 const computerCardSlot = document.querySelector('.computer-card-slot')
 const computerDeckElement = document.querySelector('.computer-deck')
 const playerDeckElement = document.querySelector('.player-deck')
@@ -49,9 +66,29 @@ function flipCards(){
 
     playerCardSlot.appendChild(playerCard.getHTML())
     computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
+
+    if (isRoundWinner(playerCard, computerCard)) {
+        text.innerText = "Win"
+        playerDeck.push(playerCard)
+        playerDeck.push(computerCard)
+    } else if (isRoundWinner(playerCard, computerCard)) {
+        text.innerText = "Win"
+        computerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    } else {
+        text.innerHTML = "Draw"
+        playerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    }
 }
 
 function updateDeckCount() {
     computerDeckElement.innerText = computerDeck.numberOfCards
     playerDeckElement.innerText = playerDeck.numberOfCards
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
 }
